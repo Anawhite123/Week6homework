1e033127aac9ba8e6056bc7e40e36f0052ff59ab
Anawhite123 <brahmbhatt.kruti5689@gmail.com>
2023-05-05 23:10:03 +0100

    homework

---
+++ src/Practice0ne.java
@@ -0,0 +1,9 @@
+public class Practice0ne {
+    public static void main(String[] args) {
+        int K = 5;
+        for (int i = 1; i<=10; i++){
+            System.out.println(K+ " * " + i + " = " + K * i);
+        }
+    }
+
+}
